b45a3d21534d1eac7ffa1830680a121427ab623e
mattkram <mkramer@anaconda.com>
2026-06-08 09:42:04 -0500

    feat: add CI/CD with hatch-vcs versioning and anaconda.org publishing

--- src/lembas_planingfsi/__init__.py
+++ src/lembas_planingfsi/__init__.py
@@ -1,13 +1,14 @@
 """Lembas plugin for planingfsi hydrodynamic simulations."""
 
+from lembas_planingfsi._version import __version__
 from lembas_planingfsi.flat_plate import PlaningPlateCase
 
-__all__ = ["PlaningPlateCase", "Plugin"]
+__all__ = ["PlaningPlateCase", "Plugin", "__version__"]
 
 
 class Plugin:
     """Lembas plugin registration."""
 
     name = "lembas-planingfsi"
-    version = "0.1.0"
+    version = __version__
     case_handlers = [PlaningPlateCase]
